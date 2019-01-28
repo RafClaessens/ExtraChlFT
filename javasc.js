d6346f2f2423b857dac7242ed9c8b1a54a751980
ClaessensRaf <claessens.raf92@gmail.com>
2019-01-28 16:27:09 +0100

    kopijn van die script aan de praat te krijgen but NEVER SURRENDER!

--- javasc.js
+++ javasc.js
@@ -1,13 +1,11 @@
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
 function plusSlides(n) {
+  clearTimeout(timeout);
   showSlides(slideIndex += n);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
+  clearTimeout(timeout);
   showSlides(slideIndex = n);
 }
 
@@ -20,9 +18,16 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none"; 
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
+  
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1} 
   slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
+  timeout = setTimeout(showSlides, 5000); // Change image every x seconds
 }
+
+var slideIndex = 1;
+var timeout;
+
+showSlides(slideIndex);
+
+// Next/previous controls
